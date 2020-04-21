7edfcf4e0591fc9f7e980690c41baddcd5197bfe
blackrings <43592038+blackrings@users.noreply.github.com>
2020-04-21 11:10:35 +0800

    Add files via upload

---
+++ vue_shop-master/src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/home',
+    component: Home
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 从哪来
+  // next() 放行 next('/login') 强制跳转
+  if (to.path === '/login') return next()
+  // 获取 token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
+export default router
